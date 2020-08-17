2fc7614fdc204c84828096278714c086216fb933
Kevin Rodriguez <_@kevinrodriguez.io>
2020-08-17 13:05:52 -0600

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
